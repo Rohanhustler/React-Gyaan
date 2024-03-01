df583695330caa078fbe85612d406303fe1ec4f4
Rohan Kumar Jha <jharohan0539@gmail.com>
2024-03-01 17:13:40 +0530

    adding some extra information

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -31,3 +31,8 @@ const reactElement = React.createElement(
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
+
+/* React uses a virtual DOM, which is a lightweight copy of the real DOM. This virtual DOM is kept in memory and is used to calculate the difference between the current state of the UI and the desired state.
+React uses a diffing algorithm to compare the virtual DOM with the real DOM. This algorithm determines the smallest set of changes that need to be made to the real DOM in order to bring it in line with the virtual DOM.
+React then updates the real DOM with the changes that were determined by the diffing algorithm.
+This process is very efficient because it only updates the parts of the DOM that have actually changed. This can lead to significant performance improvements, especially for applications with complex UIs. */
