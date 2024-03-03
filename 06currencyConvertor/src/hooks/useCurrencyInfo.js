7c86674218588fceda08ecf903a8b2e9eb91c66d
Rohan Kumar Jha <jharohan0539@gmail.com>
2024-03-03 11:20:30 +0530

    changing the url of currencies

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -5,7 +5,7 @@ function useCurrencyInfo(currency) {
     
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res));
     } , [currency])
